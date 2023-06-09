63ddaf4e074956a84a1c9e6c769eab22cb362805
emmaolondo <emmaolondo448@gmail.com>
2023-06-09 05:19:38 +0300

    print variable

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # program that imports functions from the file
-    # calculator_1.py, does some Maths, and prints the result.
+    """program that imports functions from the file
+     calculator_1.py, does some Maths, and prints the result."""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
