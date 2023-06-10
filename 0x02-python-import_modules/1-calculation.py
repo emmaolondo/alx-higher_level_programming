617fd19136a8d90b83e569d01004eaf61030a333
emmaolondo <emmaolondo448@gmail.com>
2023-06-10 13:59:15 +0300

    Calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     """program that imports functions from the file
      calculator_1.py, does some Maths, and prints the result."""
     from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
