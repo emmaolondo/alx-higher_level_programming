e8b2b20ff4cf70cf644310f9c8f01f60aef38ae0
emmaolondo <emmaolondo448@gmail.com>
2023-06-13 19:21:48 +0300

    replace integer in list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,9 @@
 # function that prints all integers in a list in a reversed order
 
 def print_reversed_list_integer(my_list=[]):
-    plen = len(my_list)
-    for i in range(plen - 1, -1, -1):
-        print("{:d}".format(my_list[i]), end="\n")
+    if my_list == None:
+        pass
+    else:
+        plen = len(my_list)
+        for i in range(plen - 1, -1, -1):
+            print("{:d}".format(my_list[i]), end="\n")
