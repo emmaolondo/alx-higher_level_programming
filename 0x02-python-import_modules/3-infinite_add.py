e24228a8ea803e378305670cadbe8e4ac68ec939
emmaolondo <emmaolondo448@gmail.com>
2023-06-14 15:04:17 +0300

    print names in a compiled file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
-    if length == 1:
+    if len(sys.argv) == 1:
         print(0)
-    elif length == 2:
+    elif len(sys.argv) == 2:
         print(sys.argv[1])
     else:
         total = 0
-        for i in range(1, length):
+        for i in range(1, len(sys.argv)):
             val = int(sys.argv[i])
             total = total + val
         print(total)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 
 if __name__ == "__main__":  # the code is not executed when imported
     import hidden_4
-    names = dir(hidden_4)
-    sort_name = sorted(n for n in name if not n.startswith("__"))
-    for n in sort_name:
-        print(n)
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
