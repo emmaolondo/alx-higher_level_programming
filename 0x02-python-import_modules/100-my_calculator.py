05696bea442448ea63778a69ca40209d288f31ce
emmaolondo <emmaolondo448@gmail.com>
2023-06-15 05:15:07 +0300

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-if len(sys.argv) != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-c = sys.argv[2]
-if c != '+' and c != '-' and c != '*' and c != '/':
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-else:
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print("{} {} {} = {}".format(a, b, sys.argv[2], add(a, b)))
-    elif sys.argv[2] == '-':
-         print("{} {} {} = {}".format(a, b, sys.argv[2], sub(a, b)))
-    elif sys.argv[2] == '*':
-         print("{} {} {} = {}".format(a, b, sys.argv[2], mul(a, b)))
-    elif sys.argv[2] == '/':
-         print("{} {} {} = {}".format(a, b, sys.argv[2], div(a, b)))
+    if len(sys.argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    else:
+        c = sys.argv[2]
+        if c != '+' and c != '-' and c != '*' and c != '/':
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        else:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            if sys.argv[2] == '+':
+                print("{} {} {} = {}".format(a, b, sys.argv[2], add(a, b)))
+            elif sys.argv[2] == '-':
+                print("{} {} {} = {}".format(a, b, sys.argv[2], sub(a, b)))
+            elif sys.argv[2] == '*':
+                print("{} {} {} = {}".format(a, b, sys.argv[2], mul(a, b)))
+            elif sys.argv[2] == '/':
+                print("{} {} {} = {}".format(a, b, sys.argv[2], div(a, b)))
 
 
