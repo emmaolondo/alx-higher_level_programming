4aa904aab03ba8df7d57263fc2dfccdcad0160e2
emmaolondo <emmaolondo448@gmail.com>
2023-06-18 11:17:35 +0300

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        m_len = len(matrix[i])
+        for j in range(m_len):
+            if j != m_len - 1:
+                end_c = ' '
+            else:
+                end_c = ''
+                print("{:d}".format(matrix[i][j], end=end_c))
+        print("")
