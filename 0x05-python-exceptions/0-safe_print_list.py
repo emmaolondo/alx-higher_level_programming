8142cbfe997cd55382e6fb4b08d43c8b16e9be5e
emmaolondo <emmaolondo448@gmail.com>
2023-06-27 03:41:45 +0300

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i_len = 0
+    try:
+        while i_len < x:
+            print("{:d}".format(my_list[i_len], end=""))
+            i_len += 1
+    except IndexError:
+        pass
+    print("")
+    return i_len
