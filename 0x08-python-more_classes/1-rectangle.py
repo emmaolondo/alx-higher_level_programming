202ba66c3f836a5bb32cac04bbe013d2cd28eb07
root <emmaolondo448@gmail.com>
2023-09-06 15:43:43 -0700

    a square is a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -38,5 +38,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = value
