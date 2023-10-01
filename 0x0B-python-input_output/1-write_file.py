3e17dec75670c2dd0614417b8f78eed856441cf0
root <emmaolondo448@gmail.com>
2023-10-01 12:56:56 -0700

    append into a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" A function that writes a string to a text file(UTF8) and returns the number of characters written """
+""" A function that writes a string to a text file(UTF8) and returns the
+number of characters written """
 
 
 def write_file(filename="", text=""):
