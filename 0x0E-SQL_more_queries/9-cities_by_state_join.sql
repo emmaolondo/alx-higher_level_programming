e9fe5286873dba61d29e65b40d82eea35d535978
root <emmaolondo448@gmail.com>
2023-10-21 13:32:41 -0700

    INNER JOIN

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+
+SELECT c.id, c.name, s.name
+FROM cities AS c
+INNER JOIN states AS s
+ON c.state_id = s.id
+ORDER BY c.id;
+
