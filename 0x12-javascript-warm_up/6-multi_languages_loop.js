7be4d4f6e747a4fdfbd674b0c6131b3bf615dfc2
emmaolondo <emmaolondo448@gmail.com>
2024-01-04 17:46:08 +0300

    delete directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// print an array of strings
-
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
-}
