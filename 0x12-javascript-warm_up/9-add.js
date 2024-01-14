44b10d59539d573b9b67037e69e5e6f6130fdfe6
emmaolondo <emmaolondo448@gmail.com>
2024-01-14 00:50:14 -0800

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,12 @@
 const process = require('process');
 const args = process.argv;
 const arg2 = args[2];
+const arg3 = args[3];
 const number = parseInt(arg2);
+const num = parseInt(arg3);
 
-if (isNaN(number)) {
-  console.log('Not a number');
+if (isNaN(number) || isNaN(num)) {
+  console.log('NaN');
 } else {
-  console.log("My number: " + number);
+	console.log(number + num);
 }
