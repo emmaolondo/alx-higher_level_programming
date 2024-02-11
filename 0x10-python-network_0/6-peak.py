eb632c8d14d5d5edc666f796b7970e1849c1bbbb
emmaolondo <emmaolondo448@gmail.com>
2024-02-10 19:51:26 -0800

    find the peak of a list and time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,15 @@
 """ A function that finds the peak of unsorted integers in a list """
 
 
-
 def find_peak(list_of_integers):
     """ find the peak """
-    if list_of_integers == []:
+    a = list_of_integers[:]
+    if a == []:
         return None
 
-    for i in range(len(list_of_integers) - 1):
-        if list_of_integers[i] >= list_of_integers[i + 1] and list_of_integers[i] >= list_of_integers[i - 1]:
-            return list_of_integers[i]
+    for i in range(len(a) - 1):
+        if a[i] >= a[i + 1] and a[i] >= a[i - 1]:
+            return a[i]
 
-    if list_of_integers[-1] >= list_of_integers[-2]:
-        return list_of_integers[-1]
+    if a[-1] >= a[-2]:
+        return a[-1]
