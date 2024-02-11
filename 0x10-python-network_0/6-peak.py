82398071883a76921365fe5bb4aa2b327b5d6f6f
emmaolondo <emmaolondo448@gmail.com>
2024-02-10 19:38:04 -0800

    find the peak of a list and time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,3 +11,6 @@ def find_peak(list_of_integers):
     for i in range(len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i + 1] and list_of_integers[i] >= list_of_integers[i - 1]:
             return list_of_integers[i]
+
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
