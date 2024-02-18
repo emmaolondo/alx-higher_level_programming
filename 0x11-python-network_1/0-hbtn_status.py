f179ede2a69ebb1b58d631066e6513b1842325d6
emmaolondo <emmaolondo448@gmail.com>
2024-02-17 17:06:41 -0800

    Fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# a Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+if __name__ == "__main__":
+    """
+    fetch a url and display the body of the response
+    """
+    fetch = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(fetch) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
