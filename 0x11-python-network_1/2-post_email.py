510def0fa92a9383d479091eda2a343f224d978e
emmaolondo <emmaolondo448@gmail.com>
2024-02-17 18:23:51 -0800

    show

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    values ={'email' : email}
+    values = {'email':  email}
     data = urllib.parse.urlencode(values).encode('ascii')
     fetch = urllib.request.Request(url, data)
     with urllib.request.urlopen(fetch) as response:
