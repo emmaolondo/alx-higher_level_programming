e74a528024321aef78ad28ce9cd269137196f749
emmaolondo <emmaolondo448.com>
2024-02-21 18:43:37 -0800

    get episode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
+  }
   console.log(`code: ${response.statusCode}`);
 });
