1f2544b7f1796d31dff8c382395c28ff7153c245
emmaolondo <emmaolondo448.com>
2024-02-21 18:11:58 -0800

    readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-// script that reads and prints the content of a file
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (error, data) {
-	console.log(error || data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
