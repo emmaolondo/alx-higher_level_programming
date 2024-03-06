8606658f1e7b9f3bb24b619b5748eccd5bd91aca
emmaolondo <emmaolondo448@gmail.com>
2024-03-06 09:51:32 +0300

    remove files

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// A script that writes a string to a file
-const fs = require('fs');
-file = process.argv[2];
-string = process.argv[3]
-fs.writeFile(file, string, error => {
-	if (error) console.log(error);
-});
