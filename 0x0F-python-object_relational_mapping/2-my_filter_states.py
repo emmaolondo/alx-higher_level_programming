b039799ef8579f52b2663020b6c1b39769546c44
emmaolondo <emmaolondo448@gmail.com>
2024-03-22 01:28:04 -0700

    filter by dtates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+"""
+Write a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where name matches
+the argument.
 """
 
 import MySQLdb
@@ -10,15 +13,16 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         sys.exit(1)
 
-    user = sys.argv[1]
-    passwd = sys.argv[2]
+    u = sys.argv[1]  # user
+    p = sys.argv[2]  # password
     db = sys.argv[3]
     state_name = sys.argv[4]
 
-    con = MySQLdb.connect(host="localhost", user=user, passwd=passwd, db=db, port=3306)
+    con = MySQLdb.connect(host="localhost", user=u, passwd=p, db=db, port=3306)
 
     cur = con.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (state_name,))
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(state_name)
+    cur.execute(query)
     results = cur.fetchall()
 
     if results:
