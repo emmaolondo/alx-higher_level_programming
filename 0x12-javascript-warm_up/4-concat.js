91c8d17be7331585576781ceaeb4266732bacc94
emmaolondo <112539342+emmaolondo@users.noreply.github.com>
2024-04-09 08:40:05 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const process = require('process');
-const args = process.argv;
-const arg2 = args[2];
-const arg3 = args[3];
-
-console.log(arg2 + ' is ' + arg3);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'
-];
-
-for (const l of lines) {
-  console.log(l);
-}
