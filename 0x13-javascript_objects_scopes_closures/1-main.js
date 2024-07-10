c4eba843597cf9aec0163df4bbdb0f9dd73a4d3a
emmaolondo <emmaolondo448@gmail.com>
2024-07-10 11:17:01 +0300

    constructor

--- 0x13-javascript_objects_scopes_closures/1-main.js
+++ 0x13-javascript_objects_scopes_closures/1-main.js
@@ -17,7 +17,6 @@ console.log(r3.width);
 console.log(r3.height);
 
 const r = new Rectangle(3, 3);
-console.log(r)
-console.log(r.width)
-console.log(r.height)
-
+console.log(r);
+console.log(r.width);
+console.log(r.height);
